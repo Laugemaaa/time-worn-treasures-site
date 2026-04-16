b2f38e09a6489c79ac3f1687802a2ac0e61ce510
Laugemaaa <help.graze@gmail.com>
2026-04-16 22:21:47 +0200

    build: update pages base path for new repo

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "development" ? "/" : "/time-worn-treasures/",
+  base: mode === "development" ? "/" : "/time-worn-treasures-site/",
   server: {
     host: "::",
     port: 8080,
